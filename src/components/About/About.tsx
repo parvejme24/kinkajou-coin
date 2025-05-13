30aedd5f6949b26a211488b92a479f774ec03877
Md Parvej <mdparvejme24@gmail.com>
2025-05-14 00:05:39 +0600

    update about us spinner

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import ABOUT_IMAGE from "../../assets/about.png";
+import Large_Logo from "../../assets/large-logo.png";
 
 export default function About() {
   const containerVariants = {
@@ -74,14 +74,20 @@ export default function About() {
             className="relative"
           >
             <div className="absolute inset-0 bg-gradient-to-r from-[#ADF99F]/20 to-transparent rounded-full blur-3xl" />
-            <motion.img
-              whileHover={{ scale: 1.05 }}
-              transition={{ duration: 0.3 }}
-              src={ABOUT_IMAGE}
-              alt="about"
-              className="relative w-full h-auto "
-              draggable={false}
-            />
+            <div className="w-full lg:max-w-[70%] mx-auto flex justify-center items-center border border-[#00CC55] rounded p-14">
+              <motion.img
+                animate={{ rotate: 360 }}
+                transition={{
+                  duration: 20,
+                  repeat: Infinity,
+                  ease: "linear",
+                }}
+                src={Large_Logo}
+                alt="about"
+                className="relative w-[200px] h-[200px]"
+                draggable={false}
+              />
+            </div>
           </motion.div>
         </motion.div>
       </div>
